3532c4eedcdcc2d22ec310bf13b2333ec3269273
Dennis Blum <dennis.blum.1989@googlemail.com>
2021-12-01 08:51:23 +0100

    Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputAsInt = input.mapNotNull { str ->
+            str.toIntOrNull()
+        }
+        return inputAsInt
+            .filterIndexed { index, currentValue ->
+                if (index == 0) {
+                    // skip
+                    return@filterIndexed false
+                }
+                val previousValue = inputAsInt[index - 1]
+                currentValue > previousValue
+            }
+            .count()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +21,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
